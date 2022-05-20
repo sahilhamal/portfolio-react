e1aaef33c1107577ab282a2c3cb90dfbdc37946a
sahilhamal <sahil_hamal@yahoo.com>
2022-05-20 19:39:40 -0400

    minor rename

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -32,7 +32,7 @@ class Header extends Component {
 
             <li>
               <a className="smoothscroll" href="#resume">
-                Resume
+                Education
               </a>
             </li>
 
