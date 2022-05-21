1f6e828284b1d628ed5a8b65fbcbf8418083b5c3
sahilhamal <sahil_hamal@yahoo.com>
2022-05-21 01:17:38 -0400

    description update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -54,7 +54,9 @@ class Header extends Component {
               <h1 className="responsive-headline">Sahil Hamal</h1>
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}</h3>
+              <h3>
+                Software Engineer/Masters in Computer Science at Virginia Tech
+              </h3>
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
